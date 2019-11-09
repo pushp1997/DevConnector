a3f4aad09af597636736d310576fe3edb138e38b
Pushp Vashisht <pushptyagi1@gmail.com>
2019-11-10 01:37:54 +0530

    Connecting to MongoDB with Mongoose

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 app = express();
 
+//Connecting
+connectDB();
+
 app.get('/', (req, res) => res.send('API Running'));
 
+//process.env.PORT will pickup the port from environment variable when deployed on Heroku
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
