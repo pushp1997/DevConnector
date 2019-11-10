d5ae1a3fc0c901331d853bf885976f17b70cc87f
Pushp Vashisht <pushptyagi1@gmail.com>
2019-11-10 13:29:28 +0530

    User Registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,10 @@
 // Handles registering users, adding users
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const { check, validationResult } = require('express-validator');
+const User = require('../../models/User');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
 // @route    POST api/users
 // @desc     Register User
@@ -18,13 +21,52 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('User route');
+    const { name, email, password } = req.body;
+
+    try {
+      // See if User exists
+      let user = await User.findOne({ email }); // email: email not required since same name
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      // Get User's Gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
+
+      // Creating the User using model
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      // Encrypt Password
+      const salt = await bcrypt.genSalt(10); // More the rounds(10), more the security
+      user.password = await bcrypt.hash(password, salt);
+
+      // Saving the the user to the database
+      await user.save();
+
+      // Return jsonwebtoken (To get the user logged in as soon as he registers)
+
+      res.send('User registered');
+    } catch (ex) {
+      console.log(ex.message);
+      res.status(500).send('Server Error');
+    }
   }
 );
 
