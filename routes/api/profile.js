cc2b4df8d9864ff6cd9f1b4d594366ab50072a78
Pushp Vashisht <pushptyagi1@gmail.com>
2019-11-16 19:53:24 +0530

    Get all profiles & profile by user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -110,4 +110,42 @@ router.post(
     }
 );
 
+// @route    GET api/profile
+// @desc     Get all profiles
+// @access   Public
+router.get('/', async (req, res) => {
+    try {
+        const profiles = await Profile.find().populate('user', [
+            'name',
+            'avatar'
+        ]);
+        res.json(profiles);
+    } catch (ex) {
+        console.error(ex.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route    GET api/profile/user/:user_id
+// @desc     Get profile by user id
+// @access   Public
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profiles = await Profile.findOne({
+            user: req.params.user_id
+        }).populate('user', ['name', 'avatar']);
+
+        if (!profile) return res.status(400).json({ msg: 'Profile not found' });
+
+        res.json(profile);
+    } catch (ex) {
+        console.error(ex.message);
+        if (ex.kind == 'ObjectId') {
+            if (!profile)
+                return res.status(400).json({ msg: 'Profile not found ' });
+        }
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
