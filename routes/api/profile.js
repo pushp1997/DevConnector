c55a082e6fca5a344ba93298015f38bf9e46b38f
Pushp Vashisht <pushptyagi1@gmail.com>
2019-11-20 15:56:49 +0530

    Delete Profile Experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -228,4 +228,30 @@ router.put(
     }
 );
 
+// @route    DELETE api/profile/experience/:exp_id
+// @desc     Delete experience from profile
+// @access   Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+    try {
+        // Fetch profile
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        // Get the index to remove
+        const removeIndex = profile.experience
+            .map(item => item.id)
+            .indexOf(req.params.exp_id);
+
+        // Remove the experience from the list of experiences
+        profile.experience.splice(removeIndex, 1);
+
+        // Save the changes to database
+        await profile.save();
+
+        res.json(profile);
+    } catch (ex) {
+        console.error(ex.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;
