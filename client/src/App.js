cfab70ee9cd394ee723037aaea7ea9783fd47d13
Pushp Vashisht <pushptyagi1@gmail.com>
2019-12-22 20:59:20 +0530

    Edit Profile

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import './App.css';
 import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
 import Dashboard from './components/dashboard/Dashboard';
 import PrivateRoute from './components/routing/PrivateRoute';
 // For Redux
@@ -51,6 +52,11 @@ const App = () => {
                                 exact
                                 path='/create-profile'
                                 component={CreateProfile}
+                            />
+                            <PrivateRoute
+                                exact
+                                path='/edit-profile'
+                                component={EditProfile}
                             />
                         </Switch>
                     </section>
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -6,6 +6,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getCurrentProfile } from '../../actions/profile';
+import DashboardActions from './DashboardActions';
 
 const Dashboard = ({
     getCurrentProfile,
@@ -25,7 +26,9 @@ const Dashboard = ({
                 <i className='fas fas-user'> Welcome {user && user.name}</i>
             </p>
             {profile !== null ? (
-                <Fragment>has</Fragment>
+                <Fragment>
+                    <DashboardActions />
+                </Fragment>
             ) : (
                 <Fragment>
                     <p>
