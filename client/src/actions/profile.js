998b4830e460de19eba5165aef5fa98f484f6a92
Pushp Vashisht <pushptyagi1@gmail.com>
2019-12-22 20:15:06 +0530

    Create Profile Action

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -20,3 +20,45 @@ export const getCurrentProfile = () => async dispatch => {
         });
     }
 };
+
+// Create or Update a Profile
+export const createProfile = (
+    FormData,
+    history,
+    edit = false
+) => async dispatch => {
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        const res = await axios.post('/api/profile', FormData, config);
+
+        dispatch({
+            type: GET_PROFILE,
+            payload: res.data
+        });
+
+        dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created'));
+
+        if (!edit) {
+            history.push('/dashboard');
+        }
+    } catch (ex) {
+        const errors = ex.response.data.errors;
+
+        if (errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: {
+                msg: ex.response.statusText,
+                status: ex.response.status
+            }
+        });
+    }
+};
